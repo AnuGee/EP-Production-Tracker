828dca955db7e53c774ed308d3a061d30354517b
AnuGee <wanuwat@hotmail.com>
2025-03-30 10:32:17 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,18 @@
-// firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// ✅ config ต้องถูกต้อง
 const firebaseConfig = {
   apiKey: "AIzaSyCPkKn0_e76UNhEfYWWdexTwe_HJOuA5u0",
   authDomain: "epproductionworkflow.firebaseapp.com",
   projectId: "epproductionworkflow",
-  storageBucket: "epproductionworkflow.firebasestorage.app",
+  storageBucket: "epproductionworkflow.appspot.com",
   messagingSenderId: "497543686415",
   appId: "1:497543686415:web:e250ad8cd994468402767a",
   measurementId: "G-HFRBR0MTBW"
 };
 
+// ✅ ต้อง initializeApp ก่อนถึงจะใช้ Firestore ได้
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
